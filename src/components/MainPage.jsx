6fb4417db3dd702ccca030101004f8e7dee729d3
suren <surenpal666@gmail.com>
2026-01-16 00:02:39 +0900

    calendar capatilize changed

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Clock } from "./Clock";
-import { Calendar } from "./Calendar";
+import { Calendar } from "./Calender";
 
 export const MainPage = () => {
   return (
     <div className="w-[350px] mx-auto mt-6 p-6 rounded-xl bg-gray-900 text-white text-center font-sans">
-      <h1 className="text-2xl font-bold mb-2">Digital Clock</h1>
+      <h1 className="text-2xl font-bold mb-2">Date-time-app</h1>
 
       <Clock />
       <Calendar />
